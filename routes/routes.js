988101468cfd9b1285eaf170c2b6eda0556d2fc6
GlaucoStarling <glaucox.x@gmail.com>
2023-04-21 11:28:26 -0300

    feat: Create a crud

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,13 @@
 const express = require("express")
-const app = express();
-const router = express.Router();
-const HomeController = require("../controllers/HomeController");
+const router = express.Router()
+const HomeController = require("../controllers/HomeController")
+const UserController = require("../controllers/UserController")
 
-router.get('/', HomeController.index);
+router.get('/', HomeController.index)
 
-module.exports = router;
+router.get('/user', UserController.index)
+router.post('/user', UserController.create)
+router.get('/user/:id', UserController.show)
+router.put('/user/:id', UserController.update)
+
+module.exports = router
