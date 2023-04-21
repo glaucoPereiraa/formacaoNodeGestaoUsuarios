a854c948eff5a9951931db75a4a8ef7853b84f29
GlaucoStarling <glaucox.x@gmail.com>
2023-04-21 11:27:58 -0300

    feat: Create a UserController

---
+++ controllers/UserController.js
@@ -0,0 +1,90 @@
+const User = require("../models/User")
+
+
+class UserController {
+
+    async index(req, res)
+    {
+        let users = await User.findAll()
+        return res.json(users)
+    }
+
+    async show(req, res)
+    {
+        let id = req.params.id
+        let user = await User.findById(id)
+
+        if (user.length <= 0) {
+            res.status(406)
+            return res.json(user)
+        }
+
+        return res.json(user)
+    }
+
+    async create(req, res)
+    {
+        let { email, name, password } = req.body
+
+        if (email == undefined) {
+            res.status(400)
+            return res.json({err: "O campo email e obritagorio"})
+        }
+
+        if (name == undefined) {
+            res.status(400)
+            return res.json({err: "O campo name e obritagorio"})
+        }
+
+        if (password == undefined) {
+            res.status(400)
+            return res.json({err: "O campo password e obritagorio"})
+        }
+
+        let user = await User.findByEmail(email)
+
+        if (user.length > 0) {
+            res.status(406)
+            return res.json({err: "Ja existe um usuario com este email"})
+        }
+
+        await User.create(email, name, password)
+
+        return res.json({message: "Usuario criado com sucesso."})    
+    }
+
+    async update(req, res)
+    {
+        let id = req.params.id
+        let { email, name, password } = req.body
+
+        if (email == undefined) {
+            res.status(400)
+            return res.json({err: "O campo email e obritagorio"})
+        }
+
+        if (name == undefined) {
+            res.status(400)
+            return res.json({err: "O campo name e obritagorio"})
+        }
+
+        if (password == undefined) {
+            res.status(400)
+            return res.json({err: "O campo password e obritagorio"})
+        }
+
+        let user = await User.findByEmail(email)
+
+        if (user.length > 0) {
+            res.status(406)
+            return res.json({err: "Ja existe um usuario com este email"})
+        }
+
+        await User.updateById(id,req.body)
+
+        return res.json({message: "Usuario atualizado com sucesso."})    
+    }
+
+}
+
+module.exports = new UserController()
